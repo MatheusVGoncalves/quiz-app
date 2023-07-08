fe749ab2c0be9e1429f920bbce5232aabaebb1bb
MatheusVGoncalves <math_negocios@hotmail.com>
2023-07-07 23:07:00 -0300

    Initial Commit

---
+++ src/screens/History/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+import { THEME } from '../../styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: THEME.COLORS.GREY_800,
+  },
+  history: {
+    padding: 32,
+    flexGrow: 1, 
+  },
+  swipeableRemove: {
+    width: 90,
+    height: 90,
+    borderRadius: 6,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  swipeableContainer: {
+    width: '100%',
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    borderRadius: 6,
+    height: 90,
+    marginBottom:12
+  }
+});
